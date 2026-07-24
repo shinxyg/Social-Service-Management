5842234db5154e85c442602102beb8e300f41afb
rence111 <rencemillares619@gmail.com>
2026-07-24 13:31:28 +0800

    feat: initial clean commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+    res.send("success entry tampil");
+});  
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-linear-to-br from-zinc-900 via-stone-800 to-neutral-900 bg-size-[400%_400%] animate-gradient-xy text-white">
+      <div className="text-center p-8 bg-zinc-950/50 backdrop-blur-md rounded-2xl border border-zinc-700/30 shadow-2xl">
+        <h1 className="text-4xl font-extrabold text-zinc-300 drop-shadow-lg tracking-wide uppercase">
+          naka set up na react 18 ko pokeng ena nyo
+        </h1>
+        <p className="mt-4 text-cyan-400 font-medium text-lg">
+          basurang lee sunod ko na sau mamaya or bukas
+        </p>
+      </div>
+    </div>
+  )
+}
